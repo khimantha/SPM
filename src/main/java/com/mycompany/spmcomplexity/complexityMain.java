80c49d889837029b939c91922af950ea7d9dd4d9
khimantha <khimantha12@gmail.com>
2019-08-10 21:20:43 +0530

    fix

--- src/main/java/com/mycompany/spmcomplexity/complexityMain.java
+++ src/main/java/com/mycompany/spmcomplexity/complexityMain.java
@@ -1,3 +1,5 @@
+package com.mycompany.spmcomplexity;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,12 +10,12 @@
  *
  * @author Krishna
  */
-public class recursioncomplexity extends javax.swing.JFrame {
+public class complexityMain extends javax.swing.JFrame {
 
     /**
      * Creates new form recursioncomplexity
      */
-    public recursioncomplexity() {
+    public complexityMain() {
         initComponents();
     }
 
@@ -133,20 +135,21 @@ public class recursioncomplexity extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(recursioncomplexity.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(complexityMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(recursioncomplexity.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(complexityMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(recursioncomplexity.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(complexityMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(recursioncomplexity.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(complexityMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new recursioncomplexity().setVisible(true);
+                new complexityMain().setVisible(true);
             }
         });
     }
